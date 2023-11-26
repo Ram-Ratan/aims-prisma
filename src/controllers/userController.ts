47ae52605e7757942f92ccca50ef97116f23fe5a
Ram Singh <ram.singh@talentmonk.com>
2023-11-27 01:05:02 +0530

    signup implemented

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "../../generated/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
@@ -14,14 +15,14 @@ export const getAllUsers = async (req: Request, res: Response) => {
 };
 
 export const getUser = async (req: Request, res: Response) => {
-  console.log("getUser called")
-  const {username, password} = req?.body;
+  console.log("getUser called");
+  const { username, password } = req?.body;
   try {
     const users = await prisma.user.findUnique({
-      where:{
+      where: {
         username: username,
-        password: password
-      }
+        password: password,
+      },
     });
     res.json(users);
   } catch (error) {
@@ -34,7 +35,7 @@ export const addMultipleUsers = async (req: Request, res: Response) => {
   try {
     const response = await prisma.user.createMany({
       data: req.body,
-      skipDuplicates: true
+      skipDuplicates: true,
     });
     res.json(response);
   } catch (error) {
@@ -52,5 +53,62 @@ export const addUser = async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { email, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        email: email,
+        username: email,
+        password: hashedPassword,
+        role: role,
+      },
+    });
+
+    switch (role) {
+      case "STUDENT":
+        const { student } = req.body;
+        await prisma.student.create({
+          data: {
+            userId: user.id,
+            fullName: student?.fullName,
+            rollNo: student?.rollNo,
+            semesterId: student?.semesterId,
+            branchId: student?.branchId,
+            mobileNo: student?.mobileNo,
+            email: user?.email,
+          },
+        });
+        break;
+      case "FACULTY":
+        const { faculty } = req.body;
+        await prisma.faculty.create({
+          data: {
+            fullName: faculty?.fullName,
+            email: user?.email,
+            userId: user?.id,
+            department: faculty?.department,
+          },
+        });
+        break;
+      case "ADMIN":
+        const { admin } = req.body;
+        await prisma.admin.create({
+          data: {
+            fullName: admin?.fullName,
+            email: user?.email,
+            userId: user?.id,
+          },
+        });
+        break;
+    }
+    res.status(201).json({ id: user.id, email: user.email });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+    console.log(error);
   }
 };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addMultipleUsers, addUser, getAllUsers, getUser } from "../controllers/userController";
+import { addMultipleUsers, addUser, getAllUsers, getUser, signup } from "../controllers/userController";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get("/get-all-users", getAllUsers);
 router.post("/get-user",getUser);
 router.post("/add-user", addUser);
 router.post("/add-multiple-user", addMultipleUsers)
+router.post("/signup",signup);
 
 export default router;
